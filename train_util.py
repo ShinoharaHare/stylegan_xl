eec1b8c9a811f9887c307b639a4a227e1838de38
ShinoharaHare <anna.shiker1822@gmail.com>
2022-08-12 08:00:23 +0800

    Update train_util.py

--- train_util.py
+++ train_util.py
@@ -42,7 +42,7 @@ class Args(SimpleNamespace):
 def main(background=False):
     path = Path(__file__).absolute()
     os.chdir(path.parent)
-    os.environ['TORCH_EXTENSIONS_DIR'] = str(path.parent.joinpath('.cache'))
+    os.environ['TORCH_EXTENSIONS_DIR'] = str(path.parent.joinpath('.cache/torch_extensions'))
 
     args = Args()
     args.cfg = 'stylegan3-t'
